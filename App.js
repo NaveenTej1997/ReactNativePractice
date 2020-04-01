bc463f6f6c2bd17aaca8e5e0718c5d217b89e013
NaveenTej1997 <51020891+NaveenTej1997@users.noreply.github.com>
2020-04-01 20:56:26 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,222 +1,70 @@
-/*import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity  } from 'react-native';
+import Header from './components/header';
+import TodoItem from './components/todoitem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
+  const [todos,setTodos]=useState(
+    [
+      {text:'buy coffee',key:'1'},
+      {text:'create an app',key:'2'},
+      {text:'play on the switch',key:'3'},
+      
+    ]);
+    const pressHandler=()=>{
+      setTodos((prevTodos)=>{
+        return prevTodos.filter(todo=>todo.key!=key);
+      });
+
+    }
+    const submitHandler=(text)=>{
+      setTodos((prevTodos)=>{
+        return [
+          {text:text,key:Math.random().toString()},
+          ...prevTodos
+        ]
+      });
+    }
+   
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World! </Text>
-      </View>
-      <View style={styles.body}>
-        <Text>Lorem ipsum <Text style={styles.boldText}>dolor</Text> sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-        <Text>Lorem ipsum dolor sit amet.</Text>
-      </View>
+      <Header/>
+     <View styles={styles.content}>
+       <AddTodo/>
+    <View styles={styles.list}>
+      <FlatList
+      data={todos}
+      renderItem={({item})=>(
+      <TodoItem item={item}/>
+      
+      )}/>
       
     </View>
+     </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
     backgroundColor: 'pink',
-    padding: 20,
+    //alignItems:  'center',
+    //justifyContent: 'center',
   },
-  body: {
-    backgroundColor: 'skyblue',
-    padding: 20,
+  content:{
+    padding:40,
+    marginLeft:40,
+    backgroundColor:'yellow',
+    
   },
-  boldText: {
-    fontWeight: 'bold',
+  list:{
+    marginTop:20,
+    backgroundColor:'yellow',
+
   }
-});*/
-
-
-/*import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-
-export default function App() {
-  const [name, setName] = useState('shaun');
-  const [person, setPerson] = useState({ name: 'mario', age: 40 });
-
-  const clickHandler = () => {
-    setName('chun-li');
-    setPerson({ name: 'luigi', age: 45 });
-  };
-
-  return (
-    <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <Text>His name is {person.name} and his age is {person.age}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title='update state' onPress={clickHandler} />  
-      </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonContainer: {
-    marginTop: 20,
-  }
-});*/
-
-
-/*import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
-
-export default function App() {
-  const [name, setName] = useState('shaun');
-  const [age, setAge] = useState('30');
-
-  return (
-    <View style={styles.container}>
-
-      <Text>Enter name:</Text>
-      <TextInput 
-        placeholder='e.g. John Doe' 
-        style={styles.input}
-        onChangeText={(value) => setName(value)} />
-
-      <Text>Enter age:</Text>
-      <TextInput 
-        placeholder='e.g. 99' 
-        style={styles.input}
-        onChangeText={(value) => setAge(value)} />
-
-      <Text style={styles.result}>name: {name}, age: {age}</Text>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  }
-});*/
-
-/*import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
-
-export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'shaun', id: '1' },
-    { name: 'yoshi', id: '2' },
-    { name: 'mario', id: '3' },
-    { name: 'luigi', id: '4' },
-    { name: 'peach', id: '5' },
-    { name: 'toad', id: '6' },
-    { name: 'bowser', id: '7' },
-  ]);
-
-  return (
-    <View style={styles.container}>
-
-      <FlatList 
-        numColumns={2}
-        keyExtractor={(item) => item.id} 
-        data={people} 
-        renderItem={({ item }) => ( 
-          <Text style={styles.item}>{item.name}</Text>
-        )}
-      />
-
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    backgroundColor: '#fff',
-  },
-  item: {
-    flex: 1,
-    marginHorizontal: 10,
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-  },
-});*/
-
-
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
-
-export default function App() {
-  const [people, setPeople] = useState([
-    { name: 'shaun', id: '1' },
-    { name: 'yoshi', id: '2' },
-    { name: 'mario', id: '3' },
-    { name: 'luigi', id: '4' },
-    { name: 'peach', id: '5' },
-    { name: 'toad', id: '6' },
-    { name: 'bowser', id: '7' },
-  ]);
-
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id  != id);
-    });
-  };
-
-  return (
-    <View style={styles.container}>
-
-      <FlatList 
-        numColumns={2}
-        keyExtractor={(item) => item.id} 
-        data={people} 
-        renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => pressHandler(item.id)}>
-            <Text style={styles.item}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    backgroundColor: '#fff',
-  },
-  item: {
-    flex: 1,
-    marginHorizontal: 10,
-    marginTop: 24,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-  },
+  
 });
+
+
