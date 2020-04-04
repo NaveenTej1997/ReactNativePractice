0b8f1aa8367b9d53a8cb30bcf5b3f6e8ab156b40
NaveenTej1997 <51020891+NaveenTej1997@users.noreply.github.com>
2020-04-04 23:06:26 +0530

    practice on forms

--- App.js
+++ App.js
@@ -1,70 +1,111 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, FlatList, TouchableOpacity  } from 'react-native';
-import Header from './components/header';
-import TodoItem from './components/todoitem';
-import AddTodo from './components/addTodo';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Image,
+  Alert
+} from 'react-native';
 
-export default function App() {
-  const [todos,setTodos]=useState(
-    [
-      {text:'buy coffee',key:'1'},
-      {text:'create an app',key:'2'},
-      {text:'play on the switch',key:'3'},
-      
-    ]);
-    const pressHandler=()=>{
-      setTodos((prevTodos)=>{
-        return prevTodos.filter(todo=>todo.key!=key);
-      });
+export default class SignUpView extends Component {
 
+  constructor(props) {
+    super(props);
+    state = {
+      fullName: '',
+      email   : '',
+      password: '',
     }
-    const submitHandler=(text)=>{
-      setTodos((prevTodos)=>{
-        return [
-          {text:text,key:Math.random().toString()},
-          ...prevTodos
-        ]
-      });
-    }
-   
-  return (
-    <View style={styles.container}>
-      <Header/>
-     <View styles={styles.content}>
-       <AddTodo/>
-    <View styles={styles.list}>
-      <FlatList
-      data={todos}
-      renderItem={({item})=>(
-      <TodoItem item={item}/>
-      
-      )}/>
-      
-    </View>
-     </View>
-    </View>
-  );
+  }
+
+  onClickListener = (viewId) => {
+    Alert.alert("Alert", "Button pressed "+viewId);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/male-user/ultraviolet/50/3498db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Full name"
+              keyboardType="email-address"
+              underlineColorAndroid='transparent'
+              onChangeText={(fullName) => this.setState({fullName})}/>
+        </View>
+
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/message/ultraviolet/50/3498db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Email"
+              keyboardType="email-address"
+              underlineColorAndroid='transparent'
+              onChangeText={(email) => this.setState({email})}/>
+        </View>
+        
+        <View style={styles.inputContainer}>
+          <Image style={styles.inputIcon} source={{uri: 'https://png.icons8.com/key-2/ultraviolet/50/3498db'}}/>
+          <TextInput style={styles.inputs}
+              placeholder="Password"
+              secureTextEntry={true}
+              underlineColorAndroid='transparent'
+              onChangeText={(password) => this.setState({password})}/>
+        </View>
+
+        <TouchableHighlight style={[styles.buttonContainer, styles.signupButton]} onPress={() => this.onClickListener('sign_up')}>
+          <Text style={styles.signUpText}>Sign up</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'pink',
-    //alignItems:  'center',
-    //justifyContent: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#00b5ec',
   },
-  content:{
-    padding:40,
-    marginLeft:40,
-    backgroundColor:'yellow',
-    
+  inputContainer: {
+      borderBottomColor: '#F5FCFF',
+      backgroundColor: '#FFFFFF',
+      borderRadius:30,
+      borderBottomWidth: 1,
+      width:250,
+      height:45,
+      marginBottom:20,
+      flexDirection: 'row',
+      alignItems:'center'
   },
-  list:{
-    marginTop:20,
-    backgroundColor:'yellow',
-
+  inputs:{
+      height:45,
+      marginLeft:16,
+      borderBottomColor: '#FFFFFF',
+      flex:1,
+  },
+  inputIcon:{
+    width:30,
+    height:30,
+    marginLeft:15,
+    justifyContent: 'center'
+  },
+  buttonContainer: {
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+  },
+  signupButton: {
+    backgroundColor: "#FF4DFF",
+  },
+  signUpText: {
+    color: 'white',
   }
-  
 });
-
-
